f1ca13df972663a42556ae991a543e5bf429b612
visontail <the051425@gmail.com>
2024-02-14 11:31:35 +0100

    item render, date format

--- src/List.jsx
+++ src/List.jsx
@@ -3,7 +3,12 @@ import ListItems from "./ListItems.jsx";
 
 function List() {
 
-    const [list, setList] = useState(["item 1", "item 2", "item 3"]);
+    const [list, setList] = useState([
+        'item',
+        6,
+        new Date(2000, 0, 1, 12, 0, 0),
+        <span key="react-element">React Element Item</span>,
+    ]);
 
     return(
         <div className="list-container">
